a02c061dd7ec8001738ff1af613dce9c71e9a73d
Kumar Aniket <ka344057@gmail.com>
2025-08-22 17:29:03 +0530

    improve error handling

--- backend/socket/fileHandler.js
+++ backend/socket/fileHandler.js
@@ -1,33 +1,32 @@
 const connectionPool = require('./../config/connection');
 
 const uploadFile = (data, cb) => {
- 
 
-  let qry = "SELECT * FROM users WHERE _id = ?";
 
-  try {
-    connectionPool.query(qry, [data._id], (err, result) => {
-      if (err) {
-        throw err;
-      }
+    let qry = "SELECT * FROM users WHERE _id = ?";
 
-      if (result.length > 0) {
-        qry = "UPDATE users SET dp = ? WHERE _id = ?";
-        connectionPool.query(qry, [data.fileData, data._id], (err, result) => {
-          if (err) {
-            throw err;
-          }
-          console.log("uloaded successfully");
-          cb({error: false,msg:"dp uploaded successfully"});
+    try {
+        connectionPool.query(qry, [data._id], (err, result) => {
+            if (err) {
+                throw err;
+            }
+
+            if (result.length > 0) {
+                qry = "UPDATE users SET dp = ? WHERE _id = ?";
+                connectionPool.query(qry, [data.fileData, data._id], (err, result) => {
+                    if (err) {
+                        throw err;
+                    }
+                    
+                    cb({ error: false, msg: "dp uploaded successfully" });
+                });
+            } else {
+                throw new Error("Something went wrong please try later");
+            }
         });
-      } else {
-        cb("User not found");
-      }
-    });
-  } catch (err) {
-    console.log(err);
-    cb({error: true,msg:"An Unknown Error Occured please try again later"});
-  }
+    } catch (err) {
+        cb({ error: true, msg: "Something went wrong please try later " });
+    }
 };
 
 module.exports = uploadFile;
