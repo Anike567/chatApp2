5638bdb40b738709e817f3f980bfc8d0d7cb8469
Kumar Aniket <ka344057@gmail.com>
2025-10-06 11:11:29 +0530

    refactor(Home and created new component Chat

---
+++ frontent/src/components/Chats.jsx
@@ -0,0 +1,160 @@
+import { useState, useEffect, useRef, useContext, useCallback } from 'react'
+import SelectedUser from './SelectedUser';
+import MessageInput from './MessageInput';
+import { SocketContext } from "../store/socketIdContext";
+import { AuthContext } from "../store/authContext";
+import Message from './Message';
+
+
+export default function Chats({ selectedUser }) {
+    const [message, setMessage] = useState("");
+    const [userStatus, setUserStatus] = useState(null);
+    const messageEndRef = useRef(null);
+    const { socket, socketId } = useContext(SocketContext);
+    const { user, token } = useContext(AuthContext).authData;
+    const [messageList, setMessageList] = useState([]);
+
+    /**
+       * Send a message via socket
+       * @param {KeyboardEvent | MouseEvent} 
+       * @returns {void}
+       */
+    const handleSend = async (e) => {
+        if (e.key === "Enter" || e.type === "click") {
+            if (!message.trim() || !selectedUser) return;
+
+            const msgPayload = {
+                token: token,
+                msg: {
+                    to: selectedUser.u__id,
+                    from: user._id,
+                    deleiverd: null,
+                    message,
+                }
+
+            };
+
+            socket.emit("message-received", msgPayload, (data) => {
+                if (data.error) {
+                    alert(data.message);
+                } else {
+                    const deleivered = data.status;
+                    if (deleivered) {
+                        msgPayload.msg.deleiverd = 'delievered';
+                    }
+                    else {
+                        msgPayload.msg.deleiverd = 'pending'
+                    }
+                }
+
+                setMessageList(prev => [...prev, msgPayload.msg]);
+                setMessage("");
+            });
+
+        }
+    };
+
+    const handleIncomingMessage = useCallback((data) => {
+        console.log(data);
+        setMessageList(prev => [...prev, data]);
+    });
+
+
+
+    const getSelectedUserStatus = useCallback(() => {
+        socket.emit("heartbeat", { userId: selectedUser.u__id }, (data) => {
+            if (data.error) {
+                alert(data.message);
+                return;
+            }
+
+            setUserStatus(data.data);
+        });
+    });
+
+    const scrollToBottom = () => {
+        messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+
+    }, [messageList]);
+
+    useEffect(() => {
+        let intervalId;
+        if (selectedUser) {
+            socket.emit("updateSocketId", { userId: user._id, socketid: socketId });
+            getSelectedUserStatus();
+
+            const payload = {
+                token,
+                data: {
+                    to: selectedUser.u__id,
+                    from: user._id
+                }
+            }
+
+            socket.emit("getMessages", payload, (data) => {
+                if (data.error) {
+                    alert(data.message);
+                }
+                setMessageList(data.savedMessages);
+            });
+
+            // Heartbeat interval
+            intervalId = setInterval(getSelectedUserStatus, 30000);
+
+
+        }
+
+        return () => {
+            clearInterval(intervalId);
+        };
+
+    }, [selectedUser]);
+
+
+    useEffect(() => {
+        socket.on("message-received", handleIncomingMessage);
+
+        return () => {
+            socket.off("message-received", handleIncomingMessage);
+        }
+    }, []);
+
+
+
+
+    return (
+        <div className="flex-1 h-full bg-gray-50 border border-gray-300 rounded-2xl shadow-sm flex flex-col">
+            {selectedUser ? (
+                <>
+                    {/* Header */}
+                    <SelectedUser user={selectedUser} userStatus={userStatus} />
+
+                    {/* Messages */}
+
+
+                    <div className="flex-1 p-6 overflow-auto space-y-3">
+                        {messageList.map((msg, index) => (
+                            <Message key={index} msg={msg} user={user} />
+                        ))}
+                        <div ref={messageEndRef}></div>
+                    </div>
+
+
+                    {/* Input */}
+                    <MessageInput setMessage={setMessage} handleSend={handleSend} message={message} />
+
+                </>
+            ) : (
+                <div className="flex-1 flex items-center justify-center text-center">
+                    <h1 className="text-xl font-semibold text-gray-500">
+                        Your messages will appear here
+                    </h1>
+                </div>
+            )}
+        </div>
+    )
+}
--- frontent/src/screen/Home.jsx
+++ frontent/src/screen/Home.jsx
@@ -2,15 +2,10 @@ import { useContext, useEffect, useRef, useState } from "react";
 import Loader from "../components/Loader";
 import { SocketContext } from "../store/socketIdContext";
 import { AuthContext } from "../store/authContext";
-import UpdateProfile from "../components/UpdateProfile";
-import ForgetPassword from "./ForgetPassword";
-import FriendRequests from "../components/FriendRequests";
-import Message from "../components/Message";
-import SelectedUser from "../components/SelectedUser";
-import MessageInput from "../components/MessageInput";
 import User from "../components/User";
 import Logout from "../components/Logout";
 import Header from "../components/Header";
+import Chats from "../components/Chats";
 
 
 /**
@@ -23,128 +18,15 @@ export default function Home() {
   const [isLoading, setLoading] = useState(true);
   const [userList, setUserList] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
-  const [message, setMessage] = useState("");
-  const [messageList, setMessageList] = useState([]);
-  const messageEndRef = useRef(null);
-  const [userStatus, setUserStatus] = useState(null);
+  
+  
+  
   const [friendsList, setFriendList] = useState([]);
 
 
-  const menuItems = [
-    { id: 1, label: "Change Profile Picture" },
-    { id: 2, label: "Change Password" },
-    { id: 3, label: "Friend Requests" },
-    { id: 4, label: "Logout" },
-  ];
-
-  const settingComponents = {
-    1: <UpdateProfile />,
-    2: <ForgetPassword />,
-    3: <FriendRequests />,
-    4: <Logout user={user} />
-  };
-
-
-  
-
-  /**
-   * Scroll chat to the latest message
-   * @returns {void}
-   */
-  const scrollToBottom = () => {
-    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-  const getSelectedUserStatus = () => {
-    socket.emit("heartbeat", { userId: selectedUser.u__id }, (data) => {
-      if (data.error) {
-        alert(data.message);
-        return;
-      }
-
-      setUserStatus(data.data);
-    });
-  }
-  // Debounced search logging
-  
-
-
-
-  /**
-   * Send a message via socket
-   * @param {KeyboardEvent | MouseEvent} 
-   * @returns {void}
-   */
-  const handleSend = async (e) => {
-    if (e.key === "Enter" || e.type === "click") {
-      if (!message.trim() || !selectedUser) return;
-
-      const msgPayload = {
-        token: token,
-        msg: {
-          to: selectedUser.u__id,
-          from: user._id,
-          deleiverd: null,
-          message,
-        }
-
-      };
-
-      socket.emit("message-received", msgPayload, (data) => {
-        if (data.error) {
-          alert(data.message);
-        } else {
-          const deleivered = data.status;
-          if (deleivered) {
-            msgPayload.msg.deleiverd = 'delievered';
-          }
-          else {
-            msgPayload.msg.deleiverd = 'pending'
-          }
-        }
-
-        setMessageList(prev => [...prev, msgPayload.msg]);
-        setMessage("");
-      });
-
-    }
-  };
-
   //Setup socket listeners and fetch initial data
 
-  useEffect(() => {
-    let intervalId;
-    if (selectedUser) {
-      socket.emit("updateSocketId", { userId: user._id, socketid: socketId });
-      getSelectedUserStatus();
-
-      const payload = {
-        token,
-        data: {
-          to: selectedUser.u__id,
-          from: user._id
-        }
-      }
-
-      socket.emit("getMessages", payload, (data) => {
-        if (data.error) {
-          alert(data.message);
-        }
-        setMessageList(data.savedMessages);
-      });
-
-      // Heartbeat interval
-      intervalId = setInterval(getSelectedUserStatus, 30000);
-
-
-    }
-
-    return () => {
-      clearInterval(intervalId); // ✅ cleanup works properly
-    };
-
-  }, [selectedUser]);
-
+  
   useEffect(() => {
     if (!socket) return;
     socket.emit("getuser", { token }, (data) => {
@@ -160,30 +42,21 @@ export default function Home() {
     });
 
 
-
-    const handleIncomingMessage = (data) => {
-      console.log(data);
-      setMessageList(prev => [...prev, data]);
-    };
-
     const handleConnectionError = (err) => {
       console.log(err.message);
       alert(err.message);
     }
-    socket.on("message-received", handleIncomingMessage);
+   
     socket.on("connect_error", handleConnectionError);
 
     return () => {
-      socket.off("message-received", handleIncomingMessage);
+      
       socket.off("connect_error", handleConnectionError);
     };
   }, [socket, socketId]);
 
   // Auto-scroll on new messages
-  useEffect(() => {
-    scrollToBottom();
-
-  }, [messageList]);
+ 
 
 
   if (isLoading) {
@@ -226,35 +99,7 @@ export default function Home() {
       </div>
 
       {/* Chat Panel */}
-      <div className="flex-1 h-full bg-gray-50 border border-gray-300 rounded-2xl shadow-sm flex flex-col">
-        {selectedUser ? (
-          <>
-            {/* Header */}
-            <SelectedUser user={selectedUser} userStatus={userStatus} />
-
-            {/* Messages */}
-
-
-            <div className="flex-1 p-6 overflow-auto space-y-3">
-              {messageList.map((msg, index) => (
-                <Message key={index} msg={msg} user={user} />
-              ))}
-              <div ref={messageEndRef}></div>
-            </div>
-
-
-            {/* Input */}
-            <MessageInput setMessage={setMessage} handleSend={handleSend} message={message} />
-
-          </>
-        ) : (
-          <div className="flex-1 flex items-center justify-center text-center">
-            <h1 className="text-xl font-semibold text-gray-500">
-              Your messages will appear here
-            </h1>
-          </div>
-        )}
-      </div>
+      {selectedUser && <Chats selectedUser={selectedUser}/>}
     </div>
   );
 }
