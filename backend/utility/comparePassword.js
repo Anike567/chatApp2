951b1181074518d1f0946d59bfcb9851172942d6
Kumar Aniket <ka344057@gmail.com>
2025-10-18 16:21:36 +0530

    resolved minor error

--- backend/utility/comparePassword.js
+++ backend/utility/comparePassword.js
@@ -1,12 +1,13 @@
 const bcrypt = require('bcrypt');
 
 const comparePassword = (plainPassword, hashedPassword) => {
+
     return new Promise((resolve, reject) => {
         bcrypt.compare(plainPassword, hashedPassword, (err, result) => {
             if (err) {
                 return reject(err);
             }
-            resolve(result); // true if match, false otherwise
+            resolve(result); 
         });
     });
 };
