922fcaa83c6187883b9c0dbe6e93a94f1c9b47b6
Kumar Aniket <ka344057@gmail.com>
2025-10-18 16:07:34 +0530

    resolved some minor issues

--- frontent/src/main.jsx
+++ frontent/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+// 👇 Only run this in development mode
+if (import.meta.env.MODE === "development") {
+  const whyDidYouRender = await import("@welldone-software/why-did-you-render");
+  whyDidYouRender.default(React, {
+    trackAllPureComponents: true,
+  });
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
