d577171f8c21de4aed2121eb8a2a596d0bc7dcb9
Apple <apple@Apples-MacBook-Air.local>
2025-11-08 17:44:31 +0530

    caching userStatus for users so that to render lastseen dont have to wait after every users switches

--- backend/socket/hearbeat.js
+++ backend/socket/hearbeat.js
@@ -1,32 +1,22 @@
 const { AppDataSource } = require('../config/data-source.js');
 const { uuidToBase64UrlSafe } = require('./../utility/base64Encoding.js');
 const { master } = require('./../config/redis.js');
+const { In } = require("typeorm");
+
 
 const heartbeat = async (data, cb) => {
+  
     try {
-        // Check in Redis (fast lookup)
-        const isUserOnline = await master.exists(uuidToBase64UrlSafe(data.userId));
-
-        if (isUserOnline) {
-            cb({
-                error: false,
-                data: {
-                    is_online: true,
-                    last_seen: null
-                }
-            });
-            return;
-        }
-
+    
         // Otherwise check in DB
         const userStatusRepo = AppDataSource.getRepository("UserStatus");
 
-        const userStatus = await userStatusRepo.findOne({
-            where: { user_id: data.userId },
-            select: ["last_seen", "is_online"]
+        const usersStatus = await userStatusRepo.find({
+            where: { user_id: In (data.usersId) },
+           
         });
-
-        cb({ error: false, data: userStatus });
+        console.log(usersStatus);
+        cb({ error: false, data: usersStatus });
     } catch (err) {
         console.error("Heartbeat error:", err);
         cb({ error: true, message: "Internal Server error occurred" });
